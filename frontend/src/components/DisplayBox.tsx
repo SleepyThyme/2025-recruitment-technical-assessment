547bd84073d0ef96fca0a3102e11c50270f1d8f6
Jian Wang <z5482749@ad.unsw.edu.au>
2025-02-27 16:06:30 +1100

    changed border rounding to match viewport

--- frontend/src/components/DisplayBox.tsx
+++ frontend/src/components/DisplayBox.tsx
@@ -10,13 +10,13 @@ const DisplayBox: React.FC<DisplayBox> = ({building, roomsAvailable, imgSrc}) =>
 
   return (
     <>
-      <div className="m-[1.5vh] overflow-hidden rounded-lg h-[56vh]">
+      <div className="m-[1.5vh] overflow-hidden rounded-[2vh] h-[56vh]">
         <img src={imgSrc} className="h-full w-full object-cover"></img>
-        <div className="flex flex-row relative h-[5.5vh] justify-self-end bottom-[55vh] bg-white mx-[1.5vh] p-[0.8vh] rounded-2xl place-items-center">
+        <div className="flex flex-row relative h-[5.5vh] justify-self-end bottom-[55vh] bg-white mx-[1vh] p-[0.8vh] rounded-2xl place-items-center">
           <img src="./src/assets/greenCircle.svg" className="h-full"></img>
           <p className="m-[1vh] text-[1.8vh]">{roomsAvailable} rooms available</p>
         </div>
-        <div className="flex relative h-[8vh] bottom-[15vh] bg-orange-500 mx-[1.5vh] rounded-md justify-between place-items-center">
+        <div className="flex relative h-[8vh] bottom-[15vh] bg-orange-500 mx-[1.5vh] rounded-[2vh] justify-between place-items-center">
           <p className="m-[3vh] text-white text-[2.5vh]">{building}</p>
         </div>
       </div>
